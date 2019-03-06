d9d0b98043b8238e1765ddcd0987d8dc0d56df34
Peter Dulacka <dulacka@gmail.com>
2019-03-06 14:58:34 +0100

    Payment counts criteria should be based on users, not payments

--- src/model/Params/NumberParam.php
+++ src/model/Params/NumberParam.php
@@ -8,8 +8,12 @@ class NumberParam extends BaseParam
 
     public function escapedConditions($key): array
     {
+        return $this->conditions($this->escapeKey($key));
+    }
+
+    public function conditions($key): array
+    {
         $where = [];
-        $key = $this->escapeKey($key);
         foreach ($this->data as $operator => $value) {
             $value = intval($value);
             if ($operator == 'gt') {
